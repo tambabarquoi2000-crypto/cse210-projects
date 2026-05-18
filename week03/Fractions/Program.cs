4987ec3765489d643104a658aeb2464cf13f9b40
tambabarquoi2000-crypto <tambabarquoi2000@gmail.com>
2026-05-18 00:07:22 -1200

    commit all recent changes

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction noPara = new Fraction();
+
+        Fraction topPara = new Fraction(5);
+
+        Fraction bothPara = new Fraction(5, 6);
+
+        noPara.SetTopValue(1);
+        noPara.SetBottomValue(1);
+
+        topPara.SetBottomValue(4);
+
+        bothPara.SetTopValue(4);
+        bothPara.SetBottomValue(11);
+
+        Console.WriteLine(bothPara.GetDecimalValue());
+
     }
 }
